2892984738e760e50d7de8f2e3ec09ae28ddddef
Joseph Davis <joseph.n.davis.98@gmail.com>
2019-07-31 18:50:59 -0500

    Change footer to show current year

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer has-background-dark">
     <div className="content has-text-centered has-text-light">
-      <p>Joseph Davis &copy; 2018</p>
+      <p>Joseph Davis &copy; {new Date().getFullYear()}</p>
     </div>
   </footer>
 );

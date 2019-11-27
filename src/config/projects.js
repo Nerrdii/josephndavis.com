f1b6918ef7d7a1251caca1de0f51f0437e84fe64
Joseph Davis <joseph.n.davis.98@gmail.com>
2019-11-27 17:18:46 -0600

    Update projects

--- src/config/projects.js
+++ src/config/projects.js
@@ -1,64 +1,58 @@
 const projects = [
   {
     name: 'Recipe Box',
-    desc: 'CRUD application to easily view & manage your favorite recipes',
+    desc: 'Recipe manager built with the MERN stack',
     repo: 'https://github.com/MrWhiteNerdy/recipe-box',
     link: 'https://mwn-recipe-box.herokuapp.com/',
     tech: 'node, express, mongo, react, redux'
   },
   {
     name: 'Movie Search',
-    desc: 'Easily search and view details for your favorite movies',
+    desc: 'Angular app to search for movies',
     repo: 'https://github.com/MrWhiteNerdy/movie-search',
-    link: 'https://ecstatic-kowalevski-f18263.netlify.com',
+    link: 'https://lucid-heyrovsky-96aab8.netlify.com',
     tech: 'angular'
   },
   {
-    id: 'weather-app',
     name: 'Weather App',
-    desc: "Displays the current weather forecast in the user's location.",
+    desc: 'Displays the current weather forecast',
     repo: 'https://github.com/MrWhiteNerdy/weather-app.git',
     link: 'https://mrwhitenerdy.github.io/weather-app',
     tech: 'html, css, javascript'
   },
   {
-    id: 'random-quote-machine',
     name: 'Random Quote Machine',
-    desc: 'Displays random quotes with the ability to tweet them.',
+    desc: 'Displays random quotes with the ability to tweet them',
     repo: 'https://github.com/MrWhiteNerdy/random-quote-machine.git',
     link: 'https://mrwhitenerdy.github.io/random-quote-machine',
     tech: 'html, css, javascript'
   },
   {
-    id: 'twitch-livestream-viewer',
     name: 'Twitch Livestream Viewer',
-    desc: 'Displays the current status of various Twitch.tv streamers.',
+    desc: 'Displays the current status of various Twitch.tv streamers',
     repo: 'https://github.com/MrWhiteNerdy/twitch-livestream-viewer.git',
     link: 'https://mrwhitenerdy.github.io/twitch-livestream-viewer',
     tech: 'html, css, javascript'
   },
   {
-    id: 'chatterbox',
     name: 'Chatterbox',
     desc:
-      'A Java network-based chat application with GUI and database connectivity.',
+      'A Java network-based chat application with GUI and database connectivity',
     repo: 'https://github.com/MrWhiteNerdy/chatterbox.git',
     link: null,
     tech: 'java, javafx, mysql'
   },
   {
-    id: 'wikipedia-viewer',
     name: 'Wikipedia Viewer',
     desc:
-      "Communicates with Wikipedia's API to show search results based on a query.",
+      "Communicates with Wikipedia's API to show search results based on a query",
     repo: 'https://github.com/MrWhiteNerdy/wikipedia-viewer.git',
     link: 'https://mrwhitenerdy.github.io/wikipedia-viewer',
     tech: 'html, css, javascript'
   },
   {
-    id: 'timestamp-microservice',
     name: 'Timestamp Microservice',
-    desc: 'Converts between Unix and natural language timestamps.',
+    desc: 'Converts between Unix and natural language timestamps',
     repo: 'https://github.com/MrWhiteNerdy/timestamp-microservice.git',
     link: 'https://mwn-timestamp-microservice.herokuapp.com/',
     tech: 'node'

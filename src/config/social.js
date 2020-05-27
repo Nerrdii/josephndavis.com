8f676dc351bfed4f8ab35cc1183d540c36ad843e
Joseph Davis <joseph.davis@hrblock.com>
2020-05-27 14:48:47 -0500

    Update linkedin url

--- src/config/social.js
+++ src/config/social.js
@@ -1,5 +1,5 @@
 export default {
-  linkedin: 'https://www.linkedin.com/in/josephdavis98/',
+  linkedin: 'https://www.linkedin.com/in/josephndavis/',
   github: 'https://github.com/MrWhiteNerdy',
   email: 'mailto:joseph@josephndavis.com'
 };

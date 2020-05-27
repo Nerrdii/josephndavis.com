8895ca66fe648779a780e90cfc6e4e0a54ce163f
Joseph Davis <joseph.davis@hrblock.com>
2020-05-27 14:58:49 -0500

    Update layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import socialLinks from '../config/social';
 
 const Footer = () => (
   <footer className="footer has-background-dark">
-    <div className="content has-text-centered has-text-light">
-      <p>Joseph Davis &copy; {new Date().getFullYear()}</p>
+    <div
+      className="content has-text-centered has-text-light is-flex"
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <span>Joseph Davis &copy; {new Date().getFullYear()}</span>
+      <div className="icons">
+        <a href={socialLinks.github} target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github-square fa-2x" />
+        </a>
+        <a
+          href={socialLinks.linkedin}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin fa-2x" />
+        </a>
+        <a href={socialLinks.email}>
+          <i className="fas fa-envelope-square fa-2x" />
+        </a>
+      </div>
     </div>
   </footer>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,20 @@
 import React from 'react';
 
 import Layout from '../components/layout';
-import socialLinks from '../config/social';
 
 const IndexPage = () => (
   <Layout>
     <section className="section has-text-light">
       <div className="container">
-        <div className="columns is-centered">
-          <div className="column is-half">
-            <h1 className="is-size-2">Hi, I'm Joseph Davis</h1>
-            <br />
-            <p className="is-size-4">
-              I am a software engineer from Kansas City, MO. I have full-stack
-              experience with Java, Angular, C#, and React. I specialize in
-              front-end development, making clean and performant user interfaces
-              that just work. If I'm not at my computer, I'm probably out
-              running or playing Ultimate Frisbee.
-            </p>
-          </div>
-        </div>
-        <div className="columns is-centered">
-          <div className="column is-half">
-            <div className="icons">
-              <a
-                href={socialLinks.github}
-                target="_blank"
-                rel="noopener noreferrer">
-                <i className="fab fa-github-square fa-2x" />
-              </a>
-              <a
-                href={socialLinks.linkedin}
-                target="_blank"
-                rel="noopener noreferrer">
-                <i className="fab fa-linkedin fa-2x" />
-              </a>
-              <a href={socialLinks.email}>
-                <i className="fas fa-envelope-square fa-2x" />
-              </a>
-            </div>
-          </div>
-        </div>
+        <h1 className="is-size-2">Hi, I'm Joseph Davis</h1>
+        <br />
+        <p className="is-size-4">
+          I am a software engineer from Kansas City, MO. I have full-stack
+          experience with Java, Angular, C#, and React. I specialize in
+          front-end development, making clean and performant user interfaces
+          that just work. If I'm not at my computer, I'm probably out running or
+          playing Ultimate Frisbee.
+        </p>
       </div>
     </section>
   </Layout>
